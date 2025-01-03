e564161831083f74d58a56a23a22f1d469844e9a
Jakub Harewicz <jakub.harewicz@outlook.com>
2025-01-03 14:24:28 +0100

    dodano asynchroniczny handler dla seederów

--- api/databaseSeeder.js
+++ api/databaseSeeder.js
@@ -3,34 +3,33 @@ const User = require('./models/User');
 const users = require('./data/Users');
 const Product = require('./models/Products');
 const products = require('./data/Products');
+const AsyncHandler = require('express-async-handler');
 
-router.post('/users', async (req,res) => {
-    try {
-        //usuń wszystko
-        await User.deleteMany({});
+router.post('/users', AsyncHandler(async (req,res) => {
+    //usuń wszystko
+    await User.deleteMany({});
 
-        //dodaj wszystko
-        const UserSeeder = await User.insertMany(users);
-        
-        res.send( {UserSeeder} );
-    } catch (error) {
-        res.status(500).json({ message: "Seeding users failed", error });
-    }
-});
+    //dodaj wszystko
+    const UserSeeder = await User.insertMany(users);
+    
+    res.send( {UserSeeder} );
+
+    res.status(500).json({ message: "Seeding users failed", error });
+
+}));
 
 
-router.post('/products', async (req,res) => {
-    try {
-        //usuń wszystko
-        await Product.deleteMany({});
+router.post('/products', AsyncHandler(async (req,res) => {
+    //usuń wszystko
+    await Product.deleteMany({});
 
-        //dodaj wszystko
-        const ProductSeeder = await Product.insertMany(products);
-        
-        res.send( {ProductSeeder} );
-    } catch (error) {
-        res.status(500).json({ message: "Seeding products failed", error });
-    }
-});
+    //dodaj wszystko
+    const ProductSeeder = await Product.insertMany(products);
+    
+    res.send( {ProductSeeder} );
+
+    res.status(500).json({ message: "Seeding products failed", error });
+
+}));
 
 module.exports = router;
