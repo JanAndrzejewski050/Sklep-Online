715fd8df0a86a77156832a110461f3c2fd883dac
Jakub Harewicz <jakub.harewicz@outlook.com>
2025-01-06 20:48:09 +0100

    dodano ui dla strony detalowej konkretnego produktu (wciąż niepołączone z bazą danych)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+// @ts-ignore
+import ProductDetail from './pages/ProductDetail'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/detail" element={<ProductDetail />} />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
