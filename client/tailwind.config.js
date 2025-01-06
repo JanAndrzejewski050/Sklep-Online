fc114f9371f8031fc0347a2f38d098d56b93ed58
Jakub Harewicz <jakub.harewicz@outlook.com>
2025-01-06 20:12:18 +0100

    dodano basicowy frontend (na razie niepołączony z backendem

---
+++ client/tailwind.config.js
@@ -0,0 +1,26 @@
+const flowbite = require("flowbite-react/tailwind");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", 
+    "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+
+    flowbite.plugin()
+  ],
+};
+
