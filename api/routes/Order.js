5aec94bff731e9c02705fae5722593db61cfea61
Jakub Harewicz <jakub.harewicz@outlook.com>
2025-01-06 18:38:10 +0100

    dodano setup płatności, fetchowanie listy zamówień i konkretnego zamówienia

--- api/routes/Order.js
+++ api/routes/Order.js
@@ -2,12 +2,13 @@ const express = require("express");
 const protect = require("../middleware/Auth");
 const orderRoute = express.Router();
 const AsyncHandler = require("express-async-handler");
+const Order = require("../models/Order")
 
 orderRoute.post("/", protect, AsyncHandler(
     async (req, res) => {
         const {
             orderItems,
-            shippingAdress,
+            shippingAddress,
             paymentMethods, shippingPrice,
             taxPrice,
             totalPrice,
@@ -16,12 +17,12 @@ orderRoute.post("/", protect, AsyncHandler(
 
         if(orderItems && orderItems.length === 0) {
             res.status(400);
-            throw new Error ('No order items found');
+            throw new Error ("No order items found");
 
         } else {
             const order = new Order( {
                 orderItems,
-            shippingAdress,
+            shippingAddress,
             paymentMethods, shippingPrice,
             taxPrice,
             totalPrice,
@@ -34,5 +35,55 @@ orderRoute.post("/", protect, AsyncHandler(
         }
     }
 ));
+
+//payment
+orderRoute.put('/:id/payment', protect, AsyncHandler(
+    async (req,res) => {
+        const order = await Order.findById(req.params.id);
+
+        if(order) {
+            order.isPaid = true;
+            order.paidAt = Date.now();
+            order.paymentResult =  {
+                id: req.body.id,
+                status:req.body.status,
+                updated_time: req.body.updated_time,
+                email_address: req.body.email_address
+            };
+            const updatedOrder = await order.save();
+            res.status(200).json(updatedOrder);
+        }
+    }
+));
+
+//fetchuj wszstkie zamówienia
+orderRoute.get("/",protect, AsyncHandler(
+
+    async (req, res) => {
+        const orders = await Order.find({user: req.user._id}).sort({_id:-1});
+
+        if(orders) {
+            res.status(200).json(orders);
+
+        } else {
+            res.status(404);
+            throw new Error("Orders not found");
+        }
+    }
+));
+
+//fetchuj  konkretne zamówienie
+orderRoute.get("/:id",protect, AsyncHandler(
+    async (req, res) => {
+        const order = await Order.findById(req.params.id).populates("user","email");
+        if(order) {
+            res.status(200).json(order);
+        }
+        else {
+            res.status(404);
+            throw new Error("Order not found");
+        }
+    }
+))
 
 module.exports = orderRoute;
